da7e5a079c373cb41d1e6b38e0dfd7b2ebc48877
Karina Timoshevich <karina03tima@gmail.com>
2025-01-30 20:08:06 +0300

    update function for find keywords

--- mtran.erl
+++ mtran.erl
@@ -1,20 +1,21 @@
 -module(mtran).
--export([read_file/0, hello/0, init_table/0, process_file/1]).
+-export([read_file/0, hello/0, init_table/0,print_names_table/0, process_file/1]).
 
 % List of C# keywords
 cs_keywords() ->
     ["abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while"].
 
-% Initialize keyword table
-init_table() ->
-    case ets:info(keyword_table) of
-        undefined ->
-            ets:new(keyword_table, [named_table, set, public]),
-            io:format("Keyword table initialized.~n");
-        _ ->
-            ets:delete_all_objects(keyword_table), % Очистка перед повторным заполнением
-            io:format("Keyword table cleared and ready for use.~n")
+% Инициализация таблицы имен с индексацией
+init_table() -> 
+    case ets:info(names_table) of
+        undefined -> 
+            ets:new(names_table, [named_table, set, public]),
+            io:format("Names table initialized.~n");
+        _ -> 
+            ets:delete_all_objects(names_table), % Очистка перед повторным заполнением
+            io:format("Names table cleared and ready for use.~n")
     end.
+
 
 % Process file function
 process_file(File) ->
@@ -22,21 +23,23 @@ process_file(File) ->
         {ok, Content} ->
             String = erlang:binary_to_list(Content),
             Tokens = string:tokens(String, " \t\n\r.,;(){}"),
-            process_tokens(Tokens);
+            process_tokens(Tokens),
+            print_names_table();  % Вывод таблицы имен после обработки
         {error, Reason} ->
             io:format("Error reading file ~s: ~s~n", [File, atom_to_list(Reason)])
     end.
 
 % Token processing function
 process_tokens([]) -> ok;
-process_tokens([Token | Rest]) ->
+process_tokens([Token | Rest]) -> 
     case lists:member(Token, cs_keywords()) of
-        true -> % If the token is a C# keyword
-            case ets:lookup(keyword_table, Token) of
-                [] -> % If token does not exist, add it
-                    ets:insert(keyword_table, {Token, "Keyword"}),
-                    io:format("Added to table: {~s, \"Keyword\"}~n", [Token]);
-                _ -> ok % If token already exists, do nothing
+        true -> % Если токен - ключевое слово
+            case ets:lookup(names_table, Token) of
+                [] -> 
+                    Index = length(ets:tab2list(names_table)) + 1, % Генерация нового индекса
+                    ets:insert(names_table, {Token, Index, "Keyword"}),
+                    io:format("Added to table: {~s, ~p, \"Keyword\"}~n", [Token, Index]);
+                _ -> ok % Если токен уже есть в таблице, ничего не добавляем
             end;
         false -> ok
     end,
@@ -53,6 +56,13 @@ read_file() ->
         {error, Reason} ->
             io:format("Error reading file ~s: ~s~n", [File, atom_to_list(Reason)])
     end.
+
+% Форматированный вывод таблицы имен
+print_names_table() ->
+    lists:foreach(fun({Token, Index, Type}) -> 
+                        io:format("Index: ~p, Token: ~s, Type: ~s~n", [Index, Token, Type])
+                  end, ets:tab2list(names_table)).
+
 
 % Greeting function
 hello() ->
