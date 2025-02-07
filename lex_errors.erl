1ae7c13796d4237b599b475a9052ccbfd7ff05cf
Karina Timoshevich <karina03tima@gmail.com>
2025-02-07 03:35:22 +0300

    add function for find errors with increment/decrement

--- lex_errors.erl
+++ lex_errors.erl
@@ -16,13 +16,16 @@ check_lines([], _, _, _) ->
     ok;
 check_lines([Line | Rest], LineNum, InString, HasError) ->
     {NewInside, Error} = analyze_line(Line, InString),
+
     if Error =:= true ->
-           io:format("Lexical error: missing closing quote before semicolon at line ~p~n", [LineNum]),
+           io:format("  -> Lexical error: missing closing quote before semicolon at line ~p~n", [LineNum]),
            check_lines(Rest, LineNum + 1, false, true);
        NewInside =:= true ->
-           io:format("Lexical error: string literal not closed at line ~p~n", [LineNum]),
+           io:format("  -> Lexical error: string literal not closed at line ~p~n", [LineNum]),
            check_lines(Rest, LineNum + 1, false, true);
        true ->
+           check_for_invalid_patterns(Line, LineNum),
+           check_for_increment_decrement_patterns(Line, LineNum),
            check_lines(Rest, LineNum + 1, false, HasError)
     end.
 
@@ -32,8 +35,24 @@ analyze_line(Line, InString) ->
                           $" ->
                               {not Inside, Error};  
                           $; when Inside ->
-                              {Inside, true};       
+                              {Inside, true};
                           _ ->
                               {Inside, Error}
                       end
               end, {InString, false}, Line).
+
+check_for_invalid_patterns(Line, LineNum) ->
+    case re:run(Line, "([+]{3,}|[\\-]{3,}|[+\\-]{3}[=]{1,})") of
+        {match, _} ->
+            io:format("  -> Lexical error: invalid pattern at line ~p: ~s~n", [LineNum, Line]);
+        nomatch -> 
+            ok
+    end.
+
+check_for_increment_decrement_patterns(Line, LineNum) ->
+    case re:run(Line, "\\w+\\+{3,}|\\w+\\-{3,}|\\w+\\+\\-{2,}") of
+        {match, _} ->
+            io:format("  -> Lexical error: invalid increment/decrement pattern at line ~p: ~s~n", [LineNum, Line]);
+        nomatch -> 
+            ok
+    end.
